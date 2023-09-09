2b02167d9916b98aa74dbe8953ca68fbe82639d7
Charlotte Forsdick <c.l.forsdick@gmail.com>
2023-09-09 11:54:28 +0200

    inbox styled

--- src/components/messages/MessagePreview.jsx
+++ src/components/messages/MessagePreview.jsx
@@ -1,17 +1,40 @@
+import React from 'react';
 import { Link } from "react-router-dom";
 
+function MessagePreview({ title, message, _id, sender, timeStamp }) {
+  const previewMessage = (message, maxLength) => {
+    if (message.length > maxLength) {
+      return message.substring(0, maxLength) + '...';
+    }
+    return message;
+  };
 
-function MessagePreview({ title, message, _id, sender }) {
-    return (
-    <div className="message-preview-container">
+  const formatDateTime = (timestamp) => {
+    const date = new Date(timestamp);
+    const options = {
+      year: '2-digit', 
+      month: '2-digit', 
+      day: '2-digit', 
+      hour: '2-digit', 
+      minute: '2-digit', 
+      hour12: true
+    };
+    return new Intl.DateTimeFormat('en-GB', options).format(date);
+  };
+
+  return (
+    <div className="inbox">
+    <div className="message-item">
+      <div className="message-time">
+        {formatDateTime(timeStamp)}
+      </div>
       <Link to={`/messages/${_id}`} className="message-link">
-        <h3 className="message-title">Title: {title}</h3>
+        <div className="message-title"> {title}</div>
       </Link>
-      <p className="message-content">Message: {message}</p>
+      <div className="message-content"> {previewMessage(message, 100)}</div>
+      </div>
     </div>
-    );
-  }
+  );
+}
 
-  
-  export default MessagePreview;
-  
+export default MessagePreview;
